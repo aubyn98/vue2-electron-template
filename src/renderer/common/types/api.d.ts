55629ad44bf3d42c0c6f241f1d253a6c0875bf66
aubyn <aubyn@vip.qq.com>
2021-12-10 13:41:46 +0800

    change utils

--- src/renderer/common/types/api.d.ts
+++ src/renderer/common/types/api.d.ts
@@ -1,14 +1,16 @@
 import Vue from 'vue'
-import { Storage } from '../utils/storage'
-import addEventListener from '../utils/addEventListener'
-import compose from '../utils/compose'
-import log from '../utils/compose'
-import debounce from '../utils/debounce'
-import throttle from '../utils/throttle'
-import copy from '../utils/copy'
-import getFile from '../utils/getFile'
-import Str from '../utils/str'
-import { request } from '../utils/http'
+import { Storage } from 'utils/storage'
+import addEventListener from 'utils/addEventListener'
+import compose from 'utils/compose'
+import log from 'utils/compose'
+import debounce from 'utils/debounce'
+import throttle from 'utils/throttle'
+import copy from 'utils/copy'
+import file from 'utils/file'
+import str from 'utils/str'
+import date from 'utils/date'
+import object from 'utils/object'
+import { request } from 'utils/http'
 interface Api {
   [index: string]: (params: any) => Promise<{
     data: any
@@ -19,8 +21,10 @@ interface Api {
 }
 
 interface Utils {
-  str:Str
-  getFile: typeof getFile
+  object: typeof object
+  date: typeof date
+  str: typeof str
+  file: typeof file
   copy: typeof copy
   compose: typeof compose
   log: typeof log
